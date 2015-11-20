545c8fa19654f55182daadc7b016322765159389
TimonPeng <TimonMiniPeng@gmail.com>
2015-11-20 13:12:16 +0800

    init v1.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # qiniu-upload
 七牛上传token相关
+
+## 文件介绍
+
+### /create/create_token.py 七牛上传token生成
+
+* 必须参数
+	* access_key
+	* secret_key
+	* scope 上传空间
+	* deadline token有效时间
+* 算法
+	* 将上传策略序列化成为json格式：json_text
+	* 对json序列化后的上传策略进行URL安全的Base64编码：urlsafe_base64_text
+	* 用secret_key对编码后的上传策略进行HMAC-SHA1加密，并且做URL安全的Base64编码：HMAC_SHA1_text
+	* 将 access_key、HMAC_SHA1_text 和 urlsafe_base64_text 用 “:” 连接起来,得到UploadToken。
---
+++ create-token/create_token.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hmac
+import hashlib
+import base64
+import time
+
+access_key = ''
+secret_key = ''
+scope = ''
+deadline = ''
+
+def put_policy_encode():
+    json_text = '{"scope":"%s","deadline":%s}' % (scope, deadline)
+    return json_text
+
+
+def urlsafe_base64_encode(text):
+    urlsafe_base64_text = base64.urlsafe_b64encode(text)
+    return urlsafe_base64_text
+
+
+def HMAC_SHA1_encode(text):
+    HMAC_SHA1_text = base64.urlsafe_b64encode(hmac.new(secret_key,text,hashlib.sha1).digest())
+    return HMAC_SHA1_text
+
+
+def create_token():
+    json_text = put_policy_encode()
+    urlsafe_base64_text = urlsafe_base64_encode(json_text)
+    HMAC_SHA1_text = HMAC_SHA1_encode(urlsafe_base64_text)
+    token = '%s:%s:%s' % (access_key, HMAC_SHA1_text, urlsafe_base64_text)
+    deadtime = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(int(deadline)))
+    print 'token : %s' % token
+    print 'dead time : %s' % deadtime
+
+
+if __name__ == '__main__':
+    create_token()
